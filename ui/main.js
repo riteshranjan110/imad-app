b9232b74bfb8c17b8a4721ceddcb6908f5c65742
Ritesh <riteshk981@gmail.com>
2018-03-23 10:58:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@
 var button = document.getElementById("click-me");
 button.onClick = function () {
     var message = document.getElenmentById("message");
-    message.innerHTML = "success"; 
+    message.innerHTML = success; 
 };
 
