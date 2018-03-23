f5643c674ae46c4f79988fd50c3dbd71b0561528
Ritesh <riteshk981@gmail.com>
2018-03-23 09:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,14 +7,3 @@ button.onClick = function (){
     button.innerHTML = 'Executed Successfully';
 };
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-var moveRight =  function () {
-    marginLeft += 10;
-    img.style.marginLeft = marginLeft + 'px';
-};
-
-
-img.onclick = function () {
-    var interval = setInterval(moveRight,100);
-};
