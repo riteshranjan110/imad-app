1fdac17266ba550c7759f36c482d18232e028400
Ritesh <riteshk981@gmail.com>
2018-03-23 09:34:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ message.innerHTML = "ritesh";
 
 var img = document.getElementById('madi');
 var marginLeft;
-var moveRight = function () {
+function moveRight () {
     marginLeft +=10;
     img.style.marginLeft = marginLeft + 'px';
-};
+}
 
-var img = document.getElementById('madi');
+
 img.onclick = function () {
     var interval = setInterval(moveRight,100);
 };
