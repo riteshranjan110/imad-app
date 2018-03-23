4299d5bf3ec8c90c1f0e1158b1fc30c7670e0aff
Ritesh <riteshk981@gmail.com>
2018-03-23 10:57:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,8 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-var moveRight =  function () {
-    marginLeft += 10;
-    img.style.marginLeft = marginLeft + 'px';
-};
-
-
-img.onclick = function () {
-    var interval = setInterval(moveRight,100);
-};
 
 
 var button = document.getElementById("click-me");
 button.onClick = function () {
     var message = document.getElenmentById("message");
-    message.innerHTML ="success"; 
+    message.innerHTML = "success"; 
 };
 
