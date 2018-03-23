28fcce1d5a081b921fa32a7726915f21a60c9a03
Ritesh <riteshk981@gmail.com>
2018-03-23 09:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,5 @@
 console.log('Loaded!');
 
 var message = document.getElementById("message");
-var button = document.getElementById("button1");
-var main = function () {
- message.innerHTML = "Hi i am ritesh";
-};
+message.innerHTML = "ritesh";
+
