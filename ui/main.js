bd13865494a44c295b83afd4c31b18e93ad7ce94
Ritesh <riteshk981@gmail.com>
2018-03-23 11:09:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 
 
-var button = document.getElementById('counter');
-var counter = 0;
+var button = document.getElementById('button1');
+
 button.onclick = function () {
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   
+    var message = document.getElementById('message');
+    message.innerHTML = 'success';
 };
 
