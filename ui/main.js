a3e4b45bb7727f67642abe87281d76e2b7ebd317
Ritesh <riteshk981@gmail.com>
2018-03-23 12:35:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function () {
    };
    
    //send the request
-   request.open('GET','riteshk981.imad.hasura-app.io/counter',true);
+   request.open('GET','http://riteshk981.imad.hasura-app.io/counter',true);
    request.send(null);
 };
 
