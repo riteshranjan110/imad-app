154a59114689ce3ce10ce8f65a4af12e6f6b20d3
Ritesh <riteshk981@gmail.com>
2018-03-25 00:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,13 +28,31 @@ var inputName = document.getElementById('input-name');
 var name = inputName.value;
 var submit = document.getElementById('submit');
 submit.onclick = function () {
-    var names = ['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
+     //create a xmlhttp object
+   var request = new XMLHttpRequest();
+   //handle the request
+   request.onreadystatechange = function () {
+       if(request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+              var names = request.responseText;
+              //converting string into array 
+              names = JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            
+            var ul = document.getElementById('name-list');
+            ul.innerHTML = list;
+            }
+        }
+   };
+   
+   //send the request
+   request.open('GET','http://riteshk981.imad.hasura-app.io/submit-name?name='+name ,true);
+   request.send(null);
     
-    var ul = document.getElementById('name-list');
-    ul.innerHTML = list;
+    
+    
 
 };
